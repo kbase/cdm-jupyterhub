e9fecd731f41a909c66abd795ba82b6b98358706
Tianhao-Gu <tgu@anl.gov>
2024-06-06 10:58:38 -0500

    fix broken tests

--- test/src/spark/utils_test.py
+++ test/src/spark/utils_test.py
@@ -95,20 +95,24 @@ def test_get_base_spark_conf():
     app_name = "test_app"
     expected_master_url = "spark://spark-master:7077"
     expected_app_name = app_name
+    executor_cores = 3
 
     with mock.patch.dict('os.environ', {}):
-        result = get_base_spark_conf(app_name)
+        result = get_base_spark_conf(app_name, executor_cores)
         assert isinstance(result, SparkConf)
         assert result.get("spark.master") == expected_master_url
         assert result.get("spark.app.name") == expected_app_name
+        assert result.get("spark.executor.cores") == executor_cores
 
 
 def test_get_base_spark_conf_with_env():
     app_name = "test_app"
     custom_master_url = "spark://custom-master:7077"
+    executor_cores = 3
 
     with mock.patch.dict('os.environ', {"SPARK_MASTER_URL": custom_master_url}):
-        result = get_base_spark_conf(app_name)
+        result = get_base_spark_conf(app_name, executor_cores)
         assert isinstance(result, SparkConf)
         assert result.get("spark.master") == custom_master_url
         assert result.get("spark.app.name") == app_name
+        assert result.get("spark.executor.cores") == executor_cores
