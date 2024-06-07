a64a11248006ff31d2cb73b281e7f1e596945bc5
Tianhao-Gu <tgu@anl.gov>
2024-06-07 18:11:42 -0500

    create dev and user notebook env

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -32,3 +32,26 @@ sed -e "s|{{POSTGRES_URL}}|${POSTGRES_URL}|g" \
     -e "s|{{POSTGRES_USER}}|${POSTGRES_USER}|g" \
     -e "s|{{POSTGRES_PASSWORD}}|${POSTGRES_PASSWORD}|g" \
     /opt/config/hive-site-template.xml > "$SPARK_HOME"/conf/hive-site.xml
+
+
+update_config() {
+    sed -i "s|{{DATANUCLEUS_AUTO_CREATE_TABLES}}|${DATANUCLEUS_AUTO_CREATE_TABLES}|g" "$SPARK_HOME"/conf/hive-site.xml
+}
+
+# Set settings based on server usage
+set_environment() {
+    local lowercase_usage_mode=${USAGE_MODE,,}  # Convert to lowercase
+
+    case "$lowercase_usage_mode" in
+        dev)
+            export DATANUCLEUS_AUTO_CREATE_TABLES=true
+            ;;
+        *)
+            export DATANUCLEUS_AUTO_CREATE_TABLES=false
+            ;;
+    esac
+    update_config
+    echo "Environment settings applied for $USAGE_MODE."
+}
+
+set_environment
