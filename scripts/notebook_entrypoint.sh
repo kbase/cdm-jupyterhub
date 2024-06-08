630df421f65010779e4e4d2465a9110e38aa481a
Tianhao-Gu <tgu@anl.gov>
2024-06-08 07:00:49 -0500

    create and use read-only minIO profile for users

--- scripts/notebook_entrypoint.sh
+++ scripts/notebook_entrypoint.sh
@@ -2,13 +2,24 @@
 
 echo "starting jupyter notebook"
 
-cd "$CDM_SHARED_DIR"
+lowercase_usage_mode=${USAGE_MODE,,}
+
+case "$lowercase_usage_mode" in
+    dev)
+        export NOTEBOOK_DIR="$CDM_SHARED_DIR"
+        ;;
+    *)
+        export NOTEBOOK_DIR="$CDM_SHARED_DIR/user_shared_workspace"
+        ;;
+esac
+
+mkdir -p "$NOTEBOOK_DIR" && cd "$NOTEBOOK_DIR"
 
 # Start Jupyter Lab
 jupyter lab --ip=0.0.0.0 \
             --port="$NOTEBOOK_PORT" \
             --no-browser \
             --allow-root \
-            --notebook-dir="$CDM_SHARED_DIR" \
+            --notebook-dir="$NOTEBOOK_DIR" \
             --ServerApp.token='' \
             --ServerApp.password=''
