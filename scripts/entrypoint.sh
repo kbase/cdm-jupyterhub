a279e6f0b2f5dfdb5a0c81bd225ce25c8d3780e1
Tianhao-Gu <tgu@anl.gov>
2024-08-29 10:35:10 -0500

    using ghcr.io/kbase img for standalone spark deployment

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,11 +2,4 @@
 
 . /opt/scripts/setup.sh
 
-if [ "$SPARK_MODE" = "notebook" ]; then
-    exec /opt/scripts/notebook_entrypoint.sh "$@"
-else
-  # In bitnami/spark Dockerfile, the entrypoint is set to /opt/bitnami/scripts/spark/entrypoint.sh and followed
-  # by CMD ["/opt/bitnami/scripts/spark/run.sh"] meaning that the entrypoint is expected the run.sh script as an argument.
-  # reference: https://github.com/bitnami/containers/blob/main/bitnami/spark/3.5/debian-12/Dockerfile#L69
-    exec /opt/bitnami/scripts/spark/entrypoint.sh "$@" /opt/bitnami/scripts/spark/run.sh
-fi
+exec /opt/scripts/notebook_entrypoint.sh "$@"
