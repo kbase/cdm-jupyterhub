156e09fe4093012dc35b86a53206d9a7624dd0c9
Tianhao-Gu <tgu@anl.gov>
2024-09-05 13:47:36 -0500

    fix injection

--- scripts/spawn_notebook.sh
+++ scripts/spawn_notebook.sh
@@ -6,4 +6,4 @@ echo "Starting Jupyter Notebook for user: $USERNAME"
 cd $JUPYTERHUB_USER_HOME/$USERNAME
 
 # Start the notebook server with current user
-exec jupyterhub-singleuser "$@"
+exec jupyterhub-singleuser
