c7102a251e9e86c388c0f73fa69d8aa20e449c67
Tianhao-Gu <tgu@anl.gov>
2024-09-13 22:42:30 -0500

    set permission to 777

--- src/jupyterhub_config/custom_spawner.py
+++ src/jupyterhub_config/custom_spawner.py
@@ -218,5 +218,6 @@ class VirtualEnvSpawner(SimpleLocalProcessSpawner):
         self.log.info(f'Add spark_user to the group of {group_name}')
         subprocess.run(['sudo', 'usermod', '-aG', group_name, 'spark_user'], check=True)
 
-        # Set directory permissions to 750: Owner (rwx), Group (r-x), Others (---)
-        subprocess.run(['sudo', 'chmod', '-R', '750', user_dir], check=True)
+        # TODO: set directory permissions to 700 or 750
+        # Set directory permissions to 777: Owner (rwx), Group (rwx), Others (rwx)
+        subprocess.run(['sudo', 'chmod', '-R', '777', user_dir], check=True)
