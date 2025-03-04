78216fa7309da0ee96b5575d60d3564df334de3c
Tianhao-Gu <tgu@anl.gov>
2025-03-04 17:31:53 -0600

    use node selector

--- config/jupyterhub_config.py
+++ config/jupyterhub_config.py
@@ -79,9 +79,9 @@ if get_bool_env('USE_KUBE_SPAWNER', True):
         pod.spec.service_account_name = "cdm-jupyterhub"
         # Add a label to the pod to identify it as a JupyterHub pod
         # Ref: https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/
-        pod.metadata.labels.update({"app": "cdm-jupyterhub"})
-        # pod.spec.dns_policy = "ClusterFirstWithHostNet"
-        pod.spec.dns_policy = "ClusterFirst"
+        pod.metadata.labels.update({"app": "cdm-jupyterhub-user",
+                                    "component": "user"})
+        pod.spec.dns_policy = "ClusterFirstWithHostNet"
         pod.spec.node_selector = {"kubernetes.io/hostname": "prodb-compute01"}
         return pod
     c.KubeSpawner.modify_pod_hook = modify_pod_hook
