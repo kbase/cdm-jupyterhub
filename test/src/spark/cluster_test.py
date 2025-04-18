514b5a9e1805c05951b3e2ac3d66298535f6a503
Tianhao-Gu <tgu@anl.gov>
2025-04-18 14:19:08 -0500

    not sure why test is commented

--- test/src/spark/cluster_test.py
+++ test/src/spark/cluster_test.py
@@ -1,11 +1,11 @@
-# """Tests for the cluster module."""
+"""Tests for the cluster module."""
 
-# from spark import cluster
+from spark import cluster
 
-# def test_cluster_imports():
-#     """Test that cluster module can be imported."""
-#     assert cluster is not None
+def test_cluster_imports():
+    """Test that cluster module can be imported."""
+    assert cluster is not None
 
-# def test_noop():
-#     """Simple placeholder test."""
-#     assert 1 == 1 
+def test_noop():
+    """Simple placeholder test."""
+    assert 1 == 1 
